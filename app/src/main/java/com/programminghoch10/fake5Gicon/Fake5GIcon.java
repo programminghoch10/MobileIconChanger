e3cd2b14299656a33fe3d6f19ec3a2af91d2e7ab
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-06-12 10:30:21 +0200

    add files

---
+++ app/src/main/java/com/programminghoch10/fake5Gicon/Fake5GIcon.java
@@ -0,0 +1,33 @@
+package com.programminghoch10.fake5Gicon;
+
+import android.content.res.XResources;
+import android.graphics.drawable.Drawable;
+
+import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+
+public class Fake5GIcon implements IXposedHookInitPackageResources {
+	@Override
+	public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
+		if (!resparam.packageName.equals("com.android.systemui")) return;
+		XposedBridge.log("Replacing 4G icon with 5G icon.");
+		
+		Drawable icon_5g = resparam.res.getDrawable(resparam.res.getIdentifier("ic_5g_mobiledata", "drawable", "com.android.systemui"));
+		Drawable icon_5gplus = resparam.res.getDrawable(resparam.res.getIdentifier("ic_5g_plus_mobiledata", "drawable", "com.android.systemui"));
+		XResources.DrawableLoader loader_5g = new XResources.DrawableLoader() {
+			@Override
+			public Drawable newDrawable(XResources res, int id) throws Throwable {
+				return icon_5g;
+			}
+		};
+		XResources.DrawableLoader loader_5gplus = new XResources.DrawableLoader() {
+			@Override
+			public Drawable newDrawable(XResources res, int id) throws Throwable {
+				return icon_5gplus;
+			}
+		};
+		resparam.res.setReplacement("com.android.systemui", "drawable", "ic_4g_mobiledata", loader_5g);
+		resparam.res.setReplacement("com.android.systemui", "drawable", "ic_4g_plus_mobiledata", loader_5gplus);
+	}
+}
